1f1d1ce3915af219b26edb22d091d5dde4c773eb
Sameh <sameh.abdelhaleem95@gmail.com>
2023-02-07 20:52:32 +0200

    add different timing for entering and exiting modal using React Transition

--- src/App.js
+++ src/App.js
@@ -4,10 +4,12 @@ import "./App.css";
 import Modal from "./components/Modal/Modal";
 import Backdrop from "./components/Backdrop/Backdrop";
 import List from "./components/List/List";
+import { Transition } from "react-transition-group";
 
 class App extends Component {
   state = {
     modalIsOpen: false,
+    showBlock: false,
   };
 
   showModal = () => {
@@ -20,7 +22,37 @@ class App extends Component {
     return (
       <div className="App">
         <h1>React Animations</h1>
+        <button
+          className="Button"
+          onClick={() =>
+            this.setState((prevState) => ({ showBlock: !prevState.showBlock }))
+          }
+        >
+          Toggle
+        </button>
+        <br />
+        <Transition
+          in={this.state.showBlock}
+          timeout={300}
+          mountOnEnter
+          unmountOnExit
+        >
+          {(state) => (
+            <div
+              style={{
+                backgroundColor: "red",
+                width: 100,
+                height: 100,
+                margin: "auto",
+                transition: "opacity 1s ease-out",
+                opacity: state == "exiting" ? 0 : 1,
+              }}
+            ></div>
+          )}
+        </Transition>
+
         <Modal show={this.state.modalIsOpen} closed={this.closeModal} />
+
         <Backdrop />
         <button className="Button" onClick={this.showModal}>
           Open Modal
